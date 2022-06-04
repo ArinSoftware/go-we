590737433de28c04009b0bded1d165447ef76035
gurcancekic <gurcan.cekic@reengen.com>
2022-06-04 23:10:36 +0300

    created template cache

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,14 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/arinsoftware/go-web/pkg/config"
 	"github.com/arinsoftware/go-web/pkg/handlers"
+	"github.com/arinsoftware/go-web/pkg/render"
 )
 
 const portNum = ":4000"
 
 func main() {
+	var app config.AppConfig
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("Can not create template cache")
+	}
+
+	app.TemplateCache = tc
 
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
